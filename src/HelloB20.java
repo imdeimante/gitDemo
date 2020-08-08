e4769189cc0c82c8a1d6e7c96126532239f5dbbe
imdeimante <imdeimante@gmail.com>
2020-08-08 00:16:01 -0500

    first commit

---
+++ src/HelloB20.java
@@ -0,0 +1,8 @@
+public class HelloB20 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello B20, it iss my pleasure to be with you!");
+
+        //added a line
+    }
+}
