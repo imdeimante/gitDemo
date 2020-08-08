2713670b2771125ee3cdd6cf24fdec3535e3a866
imdeimante <imdeimante@gmail.com>
2020-08-08 01:06:41 -0500

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,6 +2,7 @@ public class HappyB20 {
     public static void main(String[] args) {
 
         System.out.println("B20 is always happy");
-        System.out.println("update");
+
+            // a brand new line is added
     }
 }
